b6fb68122a92e948959211cffc891827e746499e
davrng <darngren@gmail.com>
2022-09-16 09:44:55 +0200

    added class object

---
+++ src/demoObject.java
@@ -0,0 +1,35 @@
+public class demoObject {
+    int height;
+    int age;
+    String name;
+
+    public int getHeight() {
+        return height;
+    }
+
+    public void setHeight(int height) {
+        this.height = height;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public demoObject(int height, int age, String name) {
+        this.height = height;
+        this.age = age;
+        this.name = name;
+    }
+}
--- src/demoday8.java
+++ src/demoday8.java
@@ -1,2 +1,7 @@
 public class demoday8 {
+    public static void main(String[] args) {
+        demoObject person1 = new demoObject(180, 26, "Peter");
+        System.out.println(person1.height);
+
+    }
 }
