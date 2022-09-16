dfc30ab52bcd90ce8ee931ffcdf4d04ec4c5bf64
davrng <darngren@gmail.com>
2022-09-16 10:38:45 +0200

    newIf

--- src/demoday8.java
+++ src/demoday8.java
@@ -1,7 +1,12 @@
 public class demoday8 {
     public static void main(String[] args) {
-        demoObject person1 = new demoObject(180, 26, "Peter");
+        demoObject person1 = new demoObject(180, 28, "Peter");
         System.out.println(person1.height);
+
+        if (person1.height == 180 && person1.age == 28){
+            System.out.println("Yes");
+        }else
+            System.out.println("No");
 
     }
 }
